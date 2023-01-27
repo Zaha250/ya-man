03bbe15dd3bb049513329348d9d7a47683613167
Zaha250 <koltunov1998@mail.ru>
2023-01-28 00:12:04 +1000

    Верстка

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+        }
+    });
+})
