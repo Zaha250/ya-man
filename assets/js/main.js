ab661168e9143de0671e6761b5e29f06b55549b6
Zaha250 <koltunov1998@mail.ru>
2023-01-28 00:41:12 +1000

    Добавлено модальное окно с видео

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,33 @@ document.addEventListener('DOMContentLoaded', () => {
             disableOnInteraction: false
         }
     });
+
+    function openModal(modalSelector) {
+        modalSelector.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    }
+
+    function closeModal(modalSelector) {
+        modalSelector.style.display = 'none';
+        document.body.style.overflow = '';
+    }
+
+    function modal(modalSelector, modalTriggerSelector) {
+        const modal = document.querySelector(modalSelector);
+        const modalBtn = document.querySelectorAll(modalTriggerSelector);
+        console.log(modalBtn)
+
+        modalBtn.forEach(trigger => {
+            trigger.addEventListener('click', () => {
+                openModal(modal);
+            });
+        });
+
+        modal.addEventListener('click', (e) => {
+            if (e.target.classList.contains('modal__close') || e.target == modal) {
+                closeModal(modal);
+            }
+        });
+    }
+    modal('.modal', '[data-modal="open"]');
 })
